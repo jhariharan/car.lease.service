0da53869b90f71098ded771ca42624a90a41176e
jitha <jitha.hariharan@aegon.nl>
2022-06-20 12:20:46 +0200

    added calculation for leaserate

--- src/main/java/com/leasecar/car/lease/service/VO/Car.java
+++ src/main/java/com/leasecar/car/lease/service/VO/Car.java
@@ -1,5 +1,8 @@
 package com.leasecar.car.lease.service.VO;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,15 +10,22 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Car {
 
+  @JsonIgnore
   private Long carId;
   private String make;
   private String model;
   private String version;
+  @JsonIgnore
   private int numberOfDoors;
+  @JsonIgnore
   private int co2Emission;
   private Long grossPrice;
   private Long netPrice;
-
+  private Long mileage;
+  private Long duration;
+  private double leaseRate;
 }
--- src/main/java/com/leasecar/car/lease/service/service/CarLeaseService.java
+++ src/main/java/com/leasecar/car/lease/service/service/CarLeaseService.java
@@ -14,14 +14,27 @@ public class CarLeaseService {
   @Autowired
   private RestTemplate restTemplate;
 
+  private static double interestRate = 4.5;
 
   public ResponseTemplateVO getCustomerWithCarDetails(final Long customerId) {
     ResponseTemplateVO responseTemplateVO = new ResponseTemplateVO();
     Customer customer = restTemplate.getForObject("http://localhost:9001/api/v1/customers/" + customerId, Customer.class);
     final Long carId = customer.getCarId();
     Car car = restTemplate.getForObject("http://localhost:9002/api/v1/cars/" + carId, Car.class);
-    responseTemplateVO.setCar(car);
+    final double leaseRate = leaseRateCalculator(car);
+    Car carDetailsWithLeaseRate = new Car();
+    carDetailsWithLeaseRate.setLeaseRate(leaseRate);
+    carDetailsWithLeaseRate.setMake(car.getMake());
+    carDetailsWithLeaseRate.setModel(car.getModel());
+    responseTemplateVO.setCar(carDetailsWithLeaseRate);
     responseTemplateVO.setCustomer(customer);
     return responseTemplateVO;
+  }
+
+  private double leaseRateCalculator(final Car car) {
+    final double mileage = car.getMileage();
+    final Long duration = car.getDuration();
+    final double netPrice = car.getNetPrice();
+    return (((mileage / 12) * duration) / netPrice) + (((interestRate / 100) * netPrice) / 12);
   }
 }
